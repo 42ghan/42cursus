bf4adae14d84947197061d11d37b63c5cf03b21a
42ghan <ghan@student.42seoul.kr>
2021-08-16 18:21:53 +0900

    header update

--- include/push_swap.h
+++ include/push_swap.h
@@ -15,20 +15,33 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+/* TODO - REMOVE!!! */
+# include <stdio.h>
 
 /* NOTE - stack element structure definition */
+
 typedef struct s_stack
 {
 	int				nbr;
-	char			*u_str;
+	int				idx;
 	struct s_stack	*prev;
 	struct s_stack	*next;
 }				t_stack;
+
+typedef struct s_ops
+{
+	int				ops_idx;
+	char			stack;
+	struct s_ops	*next;
+}				t_ops;
 
 typedef struct s_head
 {
 	struct s_stack	*start;
 	int				len;
+	int				total_len;
+	int				pivot;
+	t_ops			**ops;
 }				t_head;
 
 
@@ -41,21 +54,23 @@ char			*ft_strjoin(char const *s1, char const *s2);
 char			*hex_uitoa(unsigned int n);
 int				*arg_check(int argc, char *argv[]);
 void    		error_exit(int  code); /* NOTE - arg error : 1; malloc error : 2 */
+void			elem_to_top(t_head *head, t_stack *elem);
 int				double_arr_len(char **ptr);
 void    		free_double_arr(char **ptr);
-int				*get_qsort_key(int *nbrs);
+int				*get_sorted_array(int *nbrs);
 int				check_int_range(char *nbr);
-t_stack			*stack_pop(t_head **head);
-void			elem_to_top(t_head **head, t_stack *elem);
-void			init_stack(t_head **head, int *nbrs);
+void			init_stack(t_head *head, int *nbrs);
 unsigned int	*int_to_unsigned_arr(int *nbrs);
 int				*unsigned_to_int_arr(int *nbrs, unsigned int *u_nbrs);
 
 /* NOTE - stack operations */
-void			swap_top_two(t_head **head, int flag);
-void			push_top(t_head **from, t_head **to, int flag);
-void			rot_n_rev_rot(t_head **head, int direction, int flag);
+void			swap_top_two(t_head *head, int flag);
+void			push_top(t_head *from, t_head *to, int flag);
+void			rot_n_rev_rot(t_head *head, int direction, int flag);
 
-void	radix_sort_stacks(t_head **a_head, t_head **b_head);
+void			sort_stacks(t_head *a_head, t_head *b_head);
+
+void		write_ops(t_ops *ops_lst);
+
 
 #endif
